beccbc196455ff2e836a6845fa4bf6389b203092
emilioego <emilioego@hotmail.com>
2020-02-16 22:16:55 +0100

    Se añade la entidad application

---
+++ api/models/applicationModel.js
@@ -0,0 +1,28 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+const generate = require('nanoid/generate');
+
+var ApplicationSchema = new Schema({
+  actorId: {
+    type: Schema.Types.ObjectId
+  },
+  tickerId: {
+    type: Schema.Types.ObjectId
+  },
+  comment: {
+    type: String,
+    required: 'Kindly enter your comments'
+  },
+  status: [{
+    type: String,
+    required: 'Kindly enter the status of the application',
+    enum: ['ACCEPTED', 'DUE', 'REJECTED','CANCELLED']
+  }],
+  created: {
+    type: Date,
+    default: Date.now
+  }
+}, { strict: false });
+
+module.exports = mongoose.model('Applications', ApplicationSchema);
