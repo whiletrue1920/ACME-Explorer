d0bd208f3548cf92e7331d9219c924550d978663
emilioego <emilioego@hotmail.com>
2020-02-16 20:19:18 +0100

    Se añade la ruta de busqueda

--- api/routes/tripRoutes.js
+++ api/routes/tripRoutes.js
@@ -10,4 +10,19 @@ module.exports = function(app) {
     .get(trips.read_a_trip)
 	.put(trips.update_a_trip)
     .delete(trips.delete_a_trip);
+  
+  /**
+   * Search engine for trips
+   * Get trips depending on params
+   *    RequiredRoles: Explorer
+   *
+   * @section trip
+   * @type get
+   * @url /v1/trips/search
+   * @param {string} keyword
+   * @param {string} price_range
+   * @param {string} date_range 
+  */
+  app.route('/v1/trips/search')
+  .get(trip.search_trips);
 };
