2644df112996124a85d345276db5742eaf31c780
Enrique Guerrero <enriqueguefer@gmail.com>
2020-02-17 22:47:59 +0100

    strict true en el schema de Trip y Stage para evitar atributos extras

--- api/models/tripModel.js
+++ api/models/tripModel.js
@@ -20,7 +20,7 @@ var StageSchema = new Schema({
         type: Number,
         required: 'Kindly enter the stage price'
     }
-});
+}, { strict: true });
 
 var TripSchema = new Schema({
     ticker: {
@@ -80,7 +80,7 @@ var TripSchema = new Schema({
         ]
     },
     stages: [StageSchema]
-}, { strict: false, timestamps: true });
+}, { strict: true, timestamps: true });
 
 // Execute before each item.save() call
 TripSchema.pre('save', function(callback) {
