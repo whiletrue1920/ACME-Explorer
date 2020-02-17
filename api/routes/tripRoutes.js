9455e0e04b734f072212c281318ce66d376e70b8
emilioego <emilioego@hotmail.com>
2020-02-17 19:41:54 +0100

    Se quita de trips las búsquedas

--- api/routes/tripRoutes.js
+++ api/routes/tripRoutes.js
@@ -3,26 +3,12 @@ module.exports = function(app) {
   var trips = require('../controllers/tripController');
 
   app.route('/v1/trips')
-	.get(trips.list_all_trips)
-	.post(trips.create_a_trip);
+	  .get(trips.list_all_trips)
+	  .post(trips.create_a_trip);
 
   app.route('/v1/trips/:tripId')
     .get(trips.read_a_trip)
-	.put(trips.update_a_trip)
+	  .put(trips.update_a_trip)
     .delete(trips.delete_a_trip);
-  
-  /**
-   * Search engine for trips
-   * Get trips depending on params
-   *    RequiredRoles: Explorer
-   *
-   * @section trip
-   * @type get
-   * @url /v1/trips/search
-   * @param {string} keyword
-   * @param {string} price_range
-   * @param {string} date_range 
-  */
-  app.route('/v1/trips/search')
-  .get(trips.search_trips);
+
 };
