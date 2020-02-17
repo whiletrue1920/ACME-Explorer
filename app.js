4e05c4095ae203d680b6cc8b5e2d65adcaa6b8f0
emilioego <emilioego@hotmail.com>
2020-02-17 18:11:22 +0100

    Se modifica la conexion de mongo para que no de errores

--- app.js
+++ app.js
@@ -14,14 +14,16 @@ var mongoDBName = process.env.mongoDBName || "ACME-Explorer";
 var mongoDBURI = "mongodb://" + mongoDBHostname + ":" + mongoDBPort + "/" + mongoDBName;
 
 mongoose.connect(mongoDBURI, {
-    reconnectTries: 10,
-    reconnectInterval: 500,
+    //reconnectTries: 10,
+    //reconnectInterval: 500,
     poolSize: 10, // Up to 10 sockets
     connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     family: 4, // skip trying IPv6
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
