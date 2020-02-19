c22704889fb97a75076f45fd1737ec8b2226720a
emilioego <emilioego@hotmail.com>
2020-02-19 21:10:28 +0100

    Se añade la ruta para las busquedas

---
+++ api/routes/searchRoutes.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = function(app) {
+  var searches = require('../controllers/searchController');
+
+  app.route('/v1/searches/:actorId')
+	  .get(searches.get_search_by_user)
+    .post(searches.post_search_by_user)
+    .delete(searches.delete_search_by_user);
+};
