8a85fd28ae955e847d66b6eb1d8a35b2b5006b3d
emilioego <emilioego@hotmail.com>
2020-02-26 19:51:18 +0100

    Se añaden las rutas de las busquedas

--- api/routes/searchRoutes.js
+++ api/routes/searchRoutes.js
@@ -2,9 +2,25 @@
 module.exports = function(app) {
   var searches = require('../controllers/searchController');
 
-  app.route('/v1/searches/:actorId')
-	  .get(searches.get_search_by_user)
-    .post(searches.post_search_by_user)
-    .delete(searches.delete_search_by_user);
+    /**
+   * Search engine for trips
+   * Get trips depending on params
+   *    
+   *
+   * @section orders
+   * @type get
+   * @url /v1/orders/search
+   * @param {string} clerkId //if it is null we will include the non assigned orders
+   * @param {string} delivered (true|false)
+   * @param {string} cancelled (true|false)
+   * @param {string} sortedBy (total)
+   * @param {string} reverse (true|false)
+   * @param {string} startFrom
+   * @param {string} pageSize
+  */
+ app.route('/v1/searches')
+ .get(searches.get_search_by_user)
+ .post(searches.post_search_by_user)
+ .delete(searches.delete_search_by_user);
       
 };
