037f0ddbebf4f2079f57b77c5a0de3996ca6c69a
Enrique Guerrero <enriqueguefer@gmail.com>
2020-02-28 00:31:14 +0100

    Añadido modelo de avg,min,max y stdDesviation para las agregaciones

--- api/models/dataWareHouseModel.js
+++ api/models/dataWareHouseModel.js
@@ -2,16 +2,27 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var AvgMinMaxStdDevSchema = new Schema({
+  avg: {
+      type: Number
+  },
+  min: {
+      type: Number
+  },
+  max: {
+      type: Number
+  },
+  standard_desviation: {
+    type: Number
+}
+});
+
 var DataWareHouseSchema = new mongoose.Schema({
   tripsPerManager: [{
     type: Schema.Types.ObjectId
   }],
-  applicationsPerTrips: [{
-    type: Schema.Types.ObjectId
-  }],
-  fullPriceTrips: [{
-    type: Schema.Types.ObjectId
-  }],
+  applicationsPerTrips: [AvgMinMaxStdDevSchema],
+  fullPriceTrips: [AvgMinMaxStdDevSchema],
   ratioApplicationsPerStatus:{
     type: Number,
     max: 1,
