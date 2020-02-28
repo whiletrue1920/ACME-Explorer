c8db771bc6ed3aece3acbf105e279798150bcd6c
emilioego <emilioego@hotmail.com>
2020-02-28 12:31:39 +0100

    Se añaden los parametros de las rutas

--- api/routes/searchRoutes.js
+++ api/routes/searchRoutes.js
@@ -6,17 +6,15 @@ module.exports = function(app) {
    * Search engine for trips
    * Get trips depending on params
    *    
-   *
    * @section orders
    * @type get
-   * @url /v1/orders/search
-   * @param {string} clerkId //if it is null we will include the non assigned orders
-   * @param {string} delivered (true|false)
-   * @param {string} cancelled (true|false)
-   * @param {string} sortedBy (total)
-   * @param {string} reverse (true|false)
-   * @param {string} startFrom
-   * @param {string} pageSize
+   * @url /v1/searches
+   * @param {string} actorId 
+   * @param {string} keyword
+   * @param {string} text
+   * @param {string} date_max
+   * @param {string} price_min
+   * @param {string} price_max
   */
  app.route('/v1/searches')
  .get(searches.get_search_by_user)
