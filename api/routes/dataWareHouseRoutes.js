f74a276bd46e64676fdd1e80f7a5924fbf411b66
emilioego <emilioego@hotmail.com>
2020-03-02 18:45:56 +0100

    Se añade el v1 a las rutas del DWH

--- api/routes/dataWareHouseRoutes.js
+++ api/routes/dataWareHouseRoutes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
 	 * @param [string] rebuildPeriod
 	 * 
 	*/
-	app.route('/dataWareHouse')
+	app.route('/v1/dataWareHouse')
 	.get(dataWareHouse.list_all_indicators)
 	.post(dataWareHouse.rebuildPeriod);
 
@@ -24,6 +24,6 @@ module.exports = function(app) {
 	 * @url /dataWareHouse/latest
 	 * 
 	*/
-	app.route('/dataWareHouse/latest')
+	app.route('/v1/dataWareHouse/latest')
 	.get(dataWareHouse.last_indicator);
 };
