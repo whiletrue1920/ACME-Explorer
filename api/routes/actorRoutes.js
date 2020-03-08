8828e138fc922b35070e5b33d26fb1aca4fde19e
Enrique Guerrero <enriqueguefer@gmail.com>
2020-03-08 17:02:13 +0100

    Se actualiza el endpoint de actor con el rol correcto

--- api/routes/actorRoutes.js
+++ api/routes/actorRoutes.js
@@ -2,6 +2,7 @@
 
 module.exports = function(app) {
   var actors = require('../controllers/actorController');
+  var authController = require('../controllers/authController');
 
   app.route('/v1/actors')
 	  .get(actors.list_all_actors)
@@ -20,4 +21,20 @@ module.exports = function(app) {
 
   app.route('/v1/actors/:actorId/unban')
     .put(actors.unban_an_actor)
+
+  /**
+   * Put an actor
+   *    RequiredRoles: to be the proper actor
+   * Get an actor
+   *    RequiredRoles: any
+	 *
+	 * @section actors
+	 * @type get put
+	 * @url /v1/actors/:actorId
+  */  
+ app.route('/v2/actors/:actorId')
+ .get(actors.read_an_actor)
+ .put(authController.verifyUser(['ADMINISTRATORS']),actors.update_a_verified_actor) //Consumer y clerk no puede modificar la info de otro consumer/clerk
+     
+
 };
