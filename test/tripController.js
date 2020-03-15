80ee3c4b0f0459a00bc4c01538cb902b393b4e0d
Enrique Guerrero <enriqueguefer@gmail.com>
2020-03-15 19:51:01 +0100

    post test

--- test/tripController.js
+++ test/tripController.js
@@ -11,7 +11,7 @@ chai.use(chaiHttp);
 
 describe("TRIPS: GET methods", () => {
 
-    it('should return all the matches', (done) => {
+    it('should return all trips', (done) => {
         chai
             .request(app)
             .get('/v1/trips')
@@ -32,6 +32,47 @@ describe("TRIPS: GET methods", () => {
                 expect(res).to.have.status(200);
                 expect(res.body).to.have.property('ticker');
                 expect(res.body.ticker).to.equal("0920-BFPQ");
+                done();
+            });
+    });
+
+})
+
+describe("TRIPS: POST methods", () => {
+
+    it('should save a trip', (done) => {
+
+        let trip = {
+            "title": "My Title",
+            "description": "My description",
+            "requirements": "requirements",
+            "date_start": "2020-01-29T17:08:51.000Z",
+            "date_end": "2020-01-29T17:08:51.000Z",
+            "canceled": false,
+            "reason": "",
+            "organizedBy": "5e5bf4011c9d440000ebdb6d",
+            "stages": [{
+                "title": "first stage",
+                "description": "first stage description",
+                "price": 100
+            },{
+                "title": "second stage",
+                "description": "second stage description",
+                "price": 250
+            }]
+        }
+
+        chai
+            .request(app)
+            .post('/v1/trips')
+            .send(trip)
+            .end((err, res) => {
+                console.log(res)
+                expect(res).to.have.status(200);
+                expect(res.body).should.be.a('object');
+                expect(res.body.trip).should.have.property('title');
+                expect(res.body.trip).should.have.property('ticker');
+                expect(res.body.trip).should.have.property('description');
                 done();
             });
     });
