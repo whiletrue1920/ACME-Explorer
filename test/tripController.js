df1f86d1d2152758393cbeb8122d0f37a7ccd534
Enrique Guerrero <enriqueguefer@gmail.com>
2020-03-17 20:32:27 +0100

    Completados todos los tests de POST

--- test/tripController.js
+++ test/tripController.js
@@ -6,7 +6,6 @@ const expect = require("chai").expect
 const app = require("../app.js");
 const sinon = require("sinon");
 
-
 chai.use(chaiHttp);
 
 describe("TRIPS: GET methods", () => {
@@ -160,41 +159,64 @@ describe("TRIPS: GET methods", () => {
 
 describe("TRIPS: POST methods", () => {
 
-   /* it('should save a trip', (done) => {
+    let sandbox;
+    beforeEach(function () {
+        sandbox = sinon.createSandbox();
+    });
 
-        let trip = {
-            "title": "My Title",
-            "description": "My description",
-            "requirements": "requirements",
-            "date_start": "2020-01-29T17:08:51.000Z",
-            "date_end": "2020-01-29T17:08:51.000Z",
-            "canceled": false,
-            "reason": "",
-            "organizedBy": "5e5bf4011c9d440000ebdb6d",
-            "stages": [{
-                "title": "first stage",
-                "description": "first stage description",
-                "price": 100
-            },{
-                "title": "second stage",
-                "description": "second stage description",
-                "price": 250
-            }]
-        }
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    let trip = {
+        "title": "My Title",
+        "description": "My description",
+        "requirements": "requirements",
+        "date_start": "2020-01-29T17:08:51.000Z",
+        "date_end": "2020-01-29T17:08:51.000Z",
+        "canceled": false,
+        "organizedBy": "5e5bf4011c9d440000ebdb6d",
+        "stages": [{
+            "title": "first stage",
+            "description": "first stage description",
+            "price": 100
+        },{
+            "title": "second stage",
+            "description": "second stage description",
+            "price": 250
+        }]
+    }
+
+    it('POST /trips 200 OK', (done) => {
+
+        sandbox.mock(mongoose.Model.prototype).expects('save').yields(null, trip);
 
         chai
             .request(app)
             .post('/v1/trips')
             .send(trip)
             .end((err, res) => {
-                console.log(res)
-                expect(res).to.have.status(200);
-                expect(res.body).should.be.a('object');
-                expect(res.body.trip).should.have.property('title');
-                expect(res.body.trip).should.have.property('ticker');
-                expect(res.body.trip).should.have.property('description');
+                expect(res).to.have.status(201);
+                expect(res.body.title).to.equal('My Title');
+                expect(res.body.description).to.equal('My description');
+                expect(res.body.organizedBy).to.equal('5e5bf4011c9d440000ebdb6d');
+                expect(res.body.stages[0].title).to.equal('first stage');
                 done();
             });
-    });*/
+    });
+
+    it('POST /trips 200 OK', (done) => {
+
+        sandbox.mock(mongoose.Model.prototype).expects('save').yields(new Error(), null);
+
+        chai
+            .request(app)
+            .post('/v1/trips')
+            .send(trip)
+            .end((err, res) => {
+                expect(res).to.have.status(500);
+                done();
+            });
+    });
 
 })
